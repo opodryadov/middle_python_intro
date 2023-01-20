c203156b43a3a375db554c276796313d2b6f5c82
Oleg Podryadov <oleg_podryadov@mail.ru>
2023-01-20 12:27:22 +0500

    Исправлены стилистические ошибки

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,17 @@
 import pytest
-from main import *
+from main import greetings
 
 
-@pytest.mark.parametrize( 'name,expected', [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],)
+@pytest.mark.parametrize(
+    'name,expected',
+    [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],
+)
 def test_greeting(name: str, expected: str):
     """Текст приветствия зависит от имени."""
-    assert Greeting(name) == expected
+    assert greetings(name) == expected
+
 
 def test_capitalize():
     """Все слова в имени начинаются с большой буквы."""
     name = 'яндекс практикум'
-    assert Greeting(name) == 'Привет, Яндекс Практикум'
+    assert greetings(name) == 'Привет, Яндекс Практикум'
